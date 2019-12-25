debbaaeb59be5adaf7f0ef349534450dfc803034
Tamizhvendan S <tamizh88@gmail.com>
2019-12-25 09:02:51 +0530

    add query by multiple primary keys [ch 34]

--- src/graphqlize/lacinia/ast.clj
+++ src/graphqlize/lacinia/ast.clj
@@ -11,19 +11,20 @@
                              inf/hyphenate)]
     (if-let [raw-ns (first (filter #(c-str/starts-with? raw-entity-ident %) raw-namespaces))]
       (let [x (c-str/replace-first raw-entity-ident (str raw-ns "-") "")]
-        (if (= x raw-ns) 
+        (if (= x raw-ns)
           x
           (str raw-ns "." x)))
       raw-entity-ident)))
 
 (defn- args-to-ident-predicate [root-attr-ns args]
-  (first (map (fn [[k v]]
-                [(keyword root-attr-ns (inf/hyphenate (name k))) v]) args)))
+  (->> (map (fn [[k v]]
+              [(keyword root-attr-ns (inf/hyphenate (name k))) v]) args)
+       (apply concat)
+       vec))
 
 (defn to-eql [namespaces selection-tree args]
   (let [root-attr-ns (eql-root-attr-ns namespaces selection-tree)
-        attrs (map (comp inf/hyphenate name) (keys selection-tree))
-        eql [{(args-to-ident-predicate root-attr-ns args) 
-              (vec (map #(keyword root-attr-ns %) attrs))}]]
+        attrs        (map (comp inf/hyphenate name) (keys selection-tree))
+        eql          [{(args-to-ident-predicate root-attr-ns args) (vec (map #(keyword root-attr-ns %) attrs))}]]
     (tap> {:eql eql})
     eql))
