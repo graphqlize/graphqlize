bf06c9d4941d990b9244250187bb4660ff3c34af
Tamizhvendan S <tamizh88@gmail.com>
2020-01-01 23:50:35 +0530

    add DbAdapter protocol to interact with the database [ch 31]

--- src/graphqlize/lacinia/core.clj
+++ src/graphqlize/lacinia/core.clj
@@ -10,29 +10,32 @@
             [honeyeql.core :as heql]
             [honeyeql.debug :refer [trace>>]]))
 
-(defn- query-by-primary-key-resolver [db-spec heql-meta-data]
+(defn- query-by-primary-key-resolver [db-adapter]
   ^{:tag lacinia-resolve/ResolverResult}
   (fn [context args _]
     (let [sel-tree    (executor/selections-tree context)
-          eql         (l-eql/generate (heql-md/namespace-idents heql-meta-data) sel-tree args)
-          heql-config {:attribute {:return-as :unqualified-camel-case}}]
+          eql         (-> (heql/meta-data db-adapter)
+                          heql-md/namespace-idents
+                          (l-eql/generate sel-tree args))]
       (trace>> :lacinia-resolver {:selections-tree sel-tree
                                   :args            args})
       (try
-        (->> (heql/query-single db-spec heql-config heql-meta-data eql)
+        (->> (heql/query-single db-adapter eql)
              (trace>> :resolved-value)
              lacinia-resolve/resolve-as)
         (catch Throwable e
           (trace>> :heql-error e)
           (lacinia-resolve/resolve-as nil (lacinia-util/as-error-map e)))))))
 
-(defn- resolvers [db-spec heql-meta-data]
-  {:graphqlize/query-by-primary-key (query-by-primary-key-resolver db-spec heql-meta-data)})
+(defn- resolvers [db-adapter]
+  {:graphqlize/query-by-primary-key (query-by-primary-key-resolver db-adapter)})
 
-(defn schema [db-spec]
-  (let [heql-meta-data (heql-md/fetch db-spec)
+(defn schema [db-adapter]
+  (let [heql-config    {:attribute {:return-as :unqualified-camel-case}}
+        new-db-adapter (heql/merge-config db-adapter heql-config)
+        heql-meta-data (heql/meta-data new-db-adapter)
         gql-schema     {:objects (l-obj/generate heql-meta-data)
                         :queries (l-query/generate heql-meta-data)}]
     (trace>> :gql-schema gql-schema)
     (lacinia-schema/compile
-     (lacinia-util/attach-resolvers gql-schema (resolvers db-spec heql-meta-data)))))
+     (lacinia-util/attach-resolvers gql-schema (resolvers new-db-adapter)))))
